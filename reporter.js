f648896f9045fda3777da3e0a56590f0844be23d
Joseph Marrama <jmarrama@box.com>
2013-09-16 23:21:38 -0700

    finished obkc and surfline extraction

--- reporter.js
+++ reporter.js
@@ -6,15 +6,34 @@ var wind_dir_deg;
 var wave_height;
 var surfline_rating;
 
+function analyze_data()
+{
+	console.log("wind speed and dir:");
+	console.log(wind_speed +" mph from " +wind_dir + " " + wind_dir_deg);
+	console.log("wave height\t"+wave_height+"\tsurfline rating\t" + surfline_rating);
+
+}
+
 function extract_surfline_data(height_str, rating_str)
 {
 	// first, extract height as the average of the possible range
 	var matches = height_str.match(/\d{1,2}/g);
 	if (matches[0] && matches[1]) {
-		console.log(matches[0] + "\t" + matches[1]);
-		wave_height = (matches[0] + matches[1])/2;
+		wave_height = (parseInt(matches[0]) + parseInt(matches[1]))/2;
+	} else if (matches[0]) {
+		wave_height = parseInt(matches[0]);
 	} else {
 		console.err("Data extraction for surfline wave height failed!");
+	}
+
+	if (rating_str.match(/epic/i)) {
+		surfline_rating = 3;
+	} else if (rating_str.match(/good/i)) {
+		surfline_rating = 2;
+	} else if (rating_str.match(/fair/i)) {
+		surfline_rating = 1;
+	} else {
+		surfline_rating = 0;
 	}
 }
 
@@ -24,8 +43,6 @@ function extract_obkc_data(data)
 	var obkc_regex = /F\s*\|\s*(.*)\s*\((\d*).*@\s*(.{1,5})\smph/;
 	var matches = data.match(obkc_regex);
 	if (matches) {
-		console.log("direction: " + matches[1] + "\t" + matches[2]);
-		console.log("speed: " + matches[3]);
 		wind_speed = matches[3];
 		wind_dir = matches[1];
 		wind_dir_deg = matches[2];
@@ -53,8 +70,8 @@ page.open("http://www.ob-kc.com/images/wx2.html", function (status) {
 			var report = page.evaluate(function () {
 				return [document.querySelector('#observed-spot-conditions').innerText, document.querySelector('#observed-wave-range').innerText];
 			});
-			console.log("surfline data: " + report[0] + "\t" + report[1]);
 			extract_surfline_data(report[1], report[0]);
+			analyze_data();
 		}
 		page.close();
 		phantom.exit();
